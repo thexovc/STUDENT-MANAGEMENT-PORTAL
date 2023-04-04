d67531fb3c5b7926ffebdbca68ed55e1f5ad31cf
DevTinaTi <tinadesk8@gmail.com>
2023-04-04 17:52:19 +0100

    add registration controller and student model

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
+  .use(express.urlencoded({ extended: false }));
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(`${PORT}`);
