1690b320f84f38302765f548aed031b9198747ab
DevTinaTi <tinadesk8@gmail.com>
2023-04-06 22:34:43 +0100

    work on the errorMiddleware

--- Middlewares/errrorHandling.middleware.js
+++ Middlewares/errrorHandling.middleware.js
@@ -1,3 +1,15 @@
-const errorMiddleware = (err, req, res, next) => {};
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') });
+
+const productionFunction = (err, res) => {
+  res.status(err.statusCode).json({});
+};
+
+const errorMiddleware = (err, req, res, next) => {
+  if (process.env.NODE_ENV === 'production') {
+    productionFunction(err, res);
+  }
+};
 
 module.exports = errorMiddleware;
